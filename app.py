c98c0d007ce1d5f7ff2db09b6003f1d5b35ac751
Jobega <37122818+JosephPobega@users.noreply.github.com>
2023-11-29 14:45:14 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ user_like_post = db.Table(
 )
 
 
+
 # creates the sqlite database
 with app.app_context():
     db.create_all()
@@ -50,6 +51,7 @@ def profile(username):
     return render_template('profile.html', user=user_data, posts=user_posts)
     
 
+
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query', '')
@@ -63,11 +65,6 @@ def get_user_data(username):
         abort(404) 
     return user
 
-
-
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
 
 
 
@@ -87,6 +84,10 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -99,12 +100,12 @@ def register():
         if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
             flash('Username or email already in use', 'error')
         else:
-            new_user = User(username=username, email=email, password=password)
-            db.session.add(new_user)  # Add the new user to the database
+            new_user = User(username=request.form['username'], email=request.form['email'], password=request.form['password'])
+            db.session.add(new_user)
             db.session.commit()
             flash('Registration successful', 'success')
-            return redirect(url_for('dashboard'))
-    
+        return redirect(url_for('login'))
+
     return render_template('register.html')
 
 
